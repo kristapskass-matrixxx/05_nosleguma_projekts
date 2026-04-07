f8f2b3d424bbbbbfd50e7720031419093cb7914f
Kristaps Kass <kristaps.kass@gmail.com>
2026-04-07 23:27:16 +0300

    feat: add core main.py and data_handler.py

---
+++ data_handler.py
@@ -0,0 +1,6 @@
+import csv
+::
+:: def load_data(filename):
+::     with open(filename, newline='') as f:
+::         reader = csv.DictReader(f)
+::         return list(reader)
---
+++ main.py
@@ -0,0 +1,8 @@
+from data_handler import load_data
+::
+:: def main():
+::     data = load_data("data/input.csv")
+::     print(data)
+::
+:: if __name__ == "__main__":
+::     main()
